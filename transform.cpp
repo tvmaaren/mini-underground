40d719e2edb5f9889d579fd5073c8ca5f2638746
Thomas van Maaren <thomas.v.maaren@outlook.com>
2021-11-17 17:16:41 +0100

    Added trains

--- transform.cpp
+++ transform.cpp
@@ -6,7 +6,6 @@
 
 #include "transform.h"
 
-const Matrix identity = {{{1,0,0},{0,1,0},{0,0,1}}};
 
 void transform::drawline(SDL_Renderer* renderer, float x1, float y1, float x2, float y2, int thickness,
 		int r, int g, int b, int a){
@@ -17,13 +16,33 @@ void transform::drawline(SDL_Renderer* renderer, float x1, float y1, float x2, f
 	thickLineRGBA(renderer, x1, y1, x2, y2, thickness,r, g, b, a);
 }
 
+void transform::drawrectangle(SDL_Renderer* renderer, float x1, float y1, float x2, float y2, 
+		int r, int g, int b, int a){
+	
+	
+	float fvertices_x[] = {x1, x2, x2, x1};
+	float fvertices_y[] = {y1, y1, y2, y2};
+
+	Sint16 vertices_x[4];
+	Sint16 vertices_y[4];
+	
+	for(int i=0; i<4; i++){
+		transform_coordinates(fvertices_x+i,fvertices_y+i);
+		vertices_x[i] = fvertices_x[i];
+		vertices_y[i] = fvertices_y[i];
+	}
+	
+	filledPolygonRGBA(renderer, vertices_x, vertices_y, 4,r, g, b, a);
+
+}
+
 void transform::drawcircle(SDL_Renderer* renderer, float x, float y, float radius,
 		int r, int g, int b, int a){
 	transform_coordinates(&x, &y);
 	filledCircleRGBA(renderer, x, y, radius, r, g, b, a);
 }
 
-Matrix transform::matrix_mul(Matrix A,Matrix B){
+Matrix matrix_mul(Matrix A,Matrix B){
 	//multiplies two 3x3 matrices
 	Matrix out;
 	for(int i = 0; i<3; i++){
@@ -41,6 +60,15 @@ void transform::init(){
 	m  = identity;
 }
 
+void transform::rotate(float angle){
+	Matrix rotation_matrix ={{
+	{ cos(angle), 	-sin(angle), 	0},
+	{ sin(angle), 	cos(angle), 	0},
+	{ 0, 		0, 		1}
+	}};
+	m = matrix_mul(rotation_matrix, m);
+}
+
 void transform::translate(int x,int y){
 
 	
@@ -48,7 +76,7 @@ void transform::translate(int x,int y){
 	translation_matrix.m[0][2] = x;
 	translation_matrix.m[1][2] = y;
 
-	m = matrix_mul(m, translation_matrix);
+	m = matrix_mul(translation_matrix, m);
 }
 
 void transform::transform_coordinates(float*x, float*y){
--- transform.h
+++ transform.h
@@ -1,7 +1,12 @@
+
+
 typedef struct{
 	float m[3][3];
 }Matrix;
 
+const Matrix identity = {{{1,0,0},{0,1,0},{0,0,1}}};
+
+Matrix matrix_mul(Matrix A, Matrix B);
 class transform{
 	public:
 	Matrix m;
@@ -9,10 +14,12 @@ class transform{
 			float y2, int thickness, int r, int g, int b, int a);
 	void drawcircle(SDL_Renderer* renderer, float x, float y, float radius,
 		int r, int g, int b, int a);
+	void drawrectangle(SDL_Renderer* renderer, float x1, float y1, float x2, float y2, 
+		int r, int g, int b, int a);
 	void init();
 	void translate(int x,int y);
+	void rotate(float angle);
 
 	private:
-	Matrix matrix_mul(Matrix A, Matrix B);
 	void transform_coordinates(float *x, float *y);
 };
